463f001f1b65d5b51ba3bab1e601259fb338186c
hardentoo <hardentoo@gmail.com>
2017-02-25 21:57:27 +0300

    Initial commit

---
+++ checkio/non-unique-elements.js
@@ -0,0 +1,25 @@
+// https://js.checkio.org/mission/non-unique-elements
+
+"use strict";
+
+function nonUniqueElements(data) {
+    
+    var m = data;
+    var r = [];
+    for (var i = 0; i < m.length; i++) {
+        var bool = false;
+	    for (var j = 0; j < m.length; j++) {
+		    if ( m[i] === m [j] && i != j) bool = true; 
+	    }
+	    if (bool) r.push(m[i]); 
+    }
+    
+    return r;
+}
+
+var m = [ 3, 3, 7, 7, 4, 8, 2, 1, 6 ];
+
+console.log(m);
+console.log(nonUniqueElements(m));
+
+phantom.exit();
